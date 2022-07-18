7def06eafebc7cbd002b820dc9a2645d5ecad861
Patrick Fu <paaatrickfu@gmail.com>
2022-07-18 14:18:29 +0800

    🔥 Migrate demo to documentation

--- Assets/ZegoExpressExample/GetAppIdConfig.cs
+++
@@ -1,6 +0,0 @@
-﻿
-public class GetAppIdConfig
-{
-    public const uint appID = 1536436739;
-    public const string appSign = "7bcdeb5ec284e40eaa271a848e7f8c23b37c998b48d895f12f395495dba32f5d";
-}
